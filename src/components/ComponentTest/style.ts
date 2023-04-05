23afc97921dfda3529349dd096d37aeef306677b
luisgustavopn <luisgustavop776@gmail.com>
2023-04-05 13:08:22 -0300

    first commit

---
+++ src/components/ComponentTest/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+`;
+
+export const Teste = styled.h1`
+    width:100%;
+    background-color: red;
+    color:black;
+    text-align: left;
+    margin: 0em;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+`;
+
+export const Titulo = styled.h2`
+    color: black;
+    text-align: center;
+    margin-top: 3em;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+`;
+
+export const Button = styled.button`
+    margin-top: 10em;
+    align-content: center;
+
+`;
+
+export const Input = styled.input`
+    margin: 0 auto;
+`;
+
+export const Form = styled.form``;
+
+export const Ul = styled.ul``;
+
+export const Li = styled.li``;
+
+export const Label = styled.label``;
